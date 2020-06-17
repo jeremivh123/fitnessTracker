eef991a06d2f66b34d0b46c39b9db4a271188cfd
Jeremiah <huertajeremiah@gmail.com>
2020-06-16 19:56:42 -0700

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds147821.mlab.com:47821/heroku_fdksxlkq";
+var MONGODB_URI =  "mongodb://user1:password1@ds147821.mlab.com:47821/heroku_fdksxlkq";
 
 mongoose.connect(MONGODB_URI);
 
